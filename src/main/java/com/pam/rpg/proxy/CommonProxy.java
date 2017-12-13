cde6831fcac03f2637a8ee69a4021055ec88bbf6
MatrexsVigil <worldofro@gmail.com>
2017-12-13 11:41:58 -0500

    1.12.2

---
+++ src/main/java/com/pam/rpg/proxy/CommonProxy.java
@@ -0,0 +1,26 @@
+package com.pam.rpg.proxy;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent e) {
+
+        onBlocksAndItemsLoaded();
+    }
+
+    public void init(FMLInitializationEvent e) {
+
+    }
+
+    public void postInit(FMLPostInitializationEvent e) {
+    }
+
+    public void onBlocksAndItemsLoaded() {
+
+    }
+}
